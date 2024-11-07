1a07d52fee99cb715818731fc0b94fbee26bceef
Fahim Muntasir <fmuntasir39@gmail.com>
2024-11-07 09:27:37 +0600

    add the service part and footer

---
+++ js/main.js
@@ -0,0 +1,26 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 20,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+   
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    }  
+});
+
+
